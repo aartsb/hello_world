b87f475d2e22da37d2220f057e0958cac6f9887a
aartsb <47335890+aartsb@users.noreply.github.com>
2020-01-29 17:33:25 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//first class notch
+		//double add1;
+		//double add2;
+		//double calc
+		System.out.println("Hello World");
+	}
+
+}
